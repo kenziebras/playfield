9f711d87359452383dcdb2dab7062f24cd9198de
kenziebras <kenzie.nibras@ui.ac.id>
2025-09-08 18:59:12 +0700

    =

--- main/models.py
+++ main/models.py
@@ -31,10 +31,10 @@ class Product(models.Model):
     def __str__(self):
         return self.title
     
-    @property
-    def is_news_hot(self):
-        return self.news_views > 20
+    # @property
+    # def is_news_hot(self):
+    #     return self.news_views > 20
         
-    def increment_views(self):
-        self.news_views += 1
-        self.save()
+    # def increment_views(self):
+    #     self.news_views += 1
+    #     self.save()
